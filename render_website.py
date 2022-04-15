0d126ccee676abe2776d03e69dbb25586b8268a5
aosothra <12795376+aosothra@users.noreply.github.com>
2022-04-15 15:23:28 +0400

    stop server by KeyboardInterrupt

--- render_website.py
+++ render_website.py
@@ -2,8 +2,13 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 
 def main():
-    server = HTTPServer(('0.0.0.0', 5000), SimpleHTTPRequestHandler)
-    server.serve_forever()
+    try:
+        server = HTTPServer(('0.0.0.0', 5000), SimpleHTTPRequestHandler)
+        print(f'Serving on {server.server_address[0]}:{server.server_address[1]}')
+        server.serve_forever()        
+    except KeyboardInterrupt:
+        print('Shutting server down...')
+        server.shutdown()
 
 if __name__=="__main__":
     main()
